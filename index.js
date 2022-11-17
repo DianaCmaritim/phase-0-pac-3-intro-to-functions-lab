0ba234fc641fcb82fff476e22ea797dc20cfdb1a
Diana <maritimdiane96@gmail.com>
2022-11-18 01:29:35 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+
+  function shout(string) {
+    return string.toUpperCase();
+  }
+
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+
+
+  function logShout(string) {
+    console.log(string.toUpperCase());
+  }
+  logShout(string);
+
+
+  function logWhisper(string) {
+    console.log(string.toLowerCase());
+  }
+  logWhisper(string);
+
+
+  function sayHiToHeadphonedRoommate(string){
+
+        if (string.toLowerCase(string) ===string) {
+          return("I can\'t hear you!");
+        }
+        else if(string.toUpperCase(string) === string) {
+          return("YES INDEED!"); // codes
+        }
+        else if (string === ("Let's have dinner together!")) {
+          return ("I would love to!")
+        }
+        }
+        sayHiToHeadphonedRoommate(string);
+
+
+
+
+
+
